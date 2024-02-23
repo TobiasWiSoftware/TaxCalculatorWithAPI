2480a446c2d38e93da64c9b8eb3ff930341d177d
tobias <tobias@DESKTOP-ERQFUUT>
2024-02-23 16:19:30 +0100

    Version 2.1 - now running local, on docker desktop and docker server without changes

--- TaxCalculatorBlazorServer/Program.cs
+++ TaxCalculatorBlazorServer/Program.cs
@@ -7,6 +7,7 @@ using TaxCalculatorBlazorServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
